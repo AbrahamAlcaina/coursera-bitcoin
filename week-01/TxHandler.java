c22235feac64aeb982ed1087819a8ed2dabedada
Abraham Alcaina <abraham@weareadaptive.com>
2017-09-15 18:11:24 +0200

    chore(week-01): partial implementation of isValid

--- week-01/TxHandler.java
+++ week-01/TxHandler.java
@@ -1,12 +1,17 @@
+
+
 public class TxHandler {
 
+    private UTXOPool in;
+    private UTXOPool out;
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
      * constructor.
      */
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        this.in = utxoPool;
+        this.out = new UTXOPool (this.in);
     }
 
     /**
@@ -19,8 +24,39 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
-        // IMPLEMENT THIS
+
+        boolean allOutputsAreInUTXPool = true;
+        boolean allSignaturesAreValid = true;
+        boolean allUTXOareClaimed = true;
+        boolean allOutputsAreNotNegative = true;
+        boolean hasPositiveBalance;
+        double sumInputs = 0.0;
+        double sumOutputs = 0.0;
+        for (Transaction.Input input: tx.getInputs()) {
+            // 1
+            UTXO txToFind = new UTXO(input.prevTxHash, input.outputIndex);
+            allOutputsAreInUTXPool = allOutputsAreInUTXPool && this.out.contains(txToFind);
+            // 2 wrong public key
+            Transaction.Output out = this.out.getTxOutput(txToFind);
+            //out.address
+            allSignaturesAreValid = allSignaturesAreValid && Crypto.verifySignature(
+                    tx.getOutput(input.outputIndex).address, tx.getRawDataToSign(input.outputIndex), input.signature);
+
+            sumInputs += out.value;
+        }
+        // 3 pending
+        // 4
+        for (Transaction.Output output: tx.getOutputs()) {
+            allOutputsAreNotNegative = allOutputsAreNotNegative && (output.value >= 0);
+            sumOutputs += output.value;
+        }
+        // 5
+        hasPositiveBalance = sumInputs > sumOutputs;
+        return allOutputsAreInUTXPool && allSignaturesAreValid && allUTXOareClaimed && allOutputsAreNotNegative && hasPositiveBalance;
+
     }
+
+
 
     /**
      * Handles each epoch by receiving an unordered array of proposed transactions, checking each
@@ -29,6 +65,8 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+        Transaction [] transactions = new Transaction[]{};
+        return transactions;
     }
 
 }
